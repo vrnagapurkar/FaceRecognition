d48df407f119e361c61eb860b15d292a029dc767
vrnagapurkar <vrnagapurkar@gmail.com>
2024-01-01 13:42:52 -0600

    Push 2

--- main.py
+++ main.py
@@ -3,3 +3,54 @@ import os, sys
 import cv2
 import numpy as np
 import math
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else: 
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+    
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+    def __init__(self):
+        self.encode_faces()
+
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f'faces/{image}')
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+
+    def run_recognition(self):
+        video_capture = cv2.VideoCapture(0)
+
+        if not video_capture.isOpened():
+            sys.exit('Video source not found...')
+        
+        while True:
+            ret, frame = video_capture.read()
+
+            if self.process_current_frame:
+                small_frame = cv2.resize(frame, (0,0), fx = 0.25, fy=0.25)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame)
+
+
+
+if __name__ == '__main__':
+    fr = FaceRecognition()
