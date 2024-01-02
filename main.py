a716a311f84e9ee7a6ace4da4ee06feebb66f00d
vrnagapurkar <vrnagapurkar@gmail.com>
2024-01-01 19:27:48 -0600

    Custom 1

--- main.py
+++ main.py
@@ -45,12 +45,57 @@ class FaceRecognition:
 
             if self.process_current_frame:
                 small_frame = cv2.resize(frame, (0,0), fx = 0.25, fy=0.25)
-                rgb_small_frame = small_frame[:, :, ::-1]
+                rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 
                 self.face_locations = face_recognition.face_locations(rgb_small_frame)
                 self.face_encodings = face_recognition.face_encodings(rgb_small_frame)
 
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = 'Unknown'
+                    confidence = 'Unknown'
+
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        confidence = face_confidence(face_distances[best_match_index])
+
+                    self.face_names.append(f'{name} ({confidence})')
+
+            self.process_current_frame = not self.process_current_frame
+
+
+            # display annotations
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                print(name)
+
+                if 'Unknown' not in name and int(name[11:13]) >= 90:
+                    cv2.rectangle(frame, (left, top), (right, bottom), (10, 255, 0), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (10, 255, 0), -1)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 0), 1)
+                else:
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+        
+            cv2.imshow('Face Recognition', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+            
+        video_capture.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     fr = FaceRecognition()
+    fr.run_recognition()
